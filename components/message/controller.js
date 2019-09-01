e1e05d6ab91508f90d267d3891cee257ef6aee2c
Dener Miranda <denner.miranda@gmail.com>
2019-09-01 10:32:36 -0300

    Email sender working

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,8 +1,13 @@
 const validation            = require('./validation');
 const messageDAO            = require('./dao');
 const sns                   = require('@moneyou/lib/sns');
+const ses                   = require('@moneyou/lib/ses');
+const Message               = require('@moneyou/models/message');
 
 const ValidationError       = require('./errors/validation');
+
+const SMS_DEFAULT_IDENTIFIER = 'SMS';
+const EMAIL_DEFAULT_IDENTIFIER = 'EMAIL';
 
 module.exports = {
     send: async (data) => {
@@ -10,9 +15,14 @@ module.exports = {
         if (validationResult.error) {
 			throw new ValidationError(validationResult.error);
         }
-        const message = validationResult.value;
-        if (message.recipientTypes.includes('SMS')) {
+        const message = Message.create(validationResult.value);
+        console.log(message);
+        if (message.recipientTypes.includes(SMS_DEFAULT_IDENTIFIER)) {
             await sns.sendSMS(message.phoneNumber, message.content);
+        }
+
+        if (message.recipientTypes.includes(EMAIL_DEFAULT_IDENTIFIER)) {
+            await ses.sendEmail(message.email, message.subject, message.content);
         }
         return messageDAO.create(message);
     },
